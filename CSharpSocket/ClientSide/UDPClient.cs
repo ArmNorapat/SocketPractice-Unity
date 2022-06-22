20fda06651d89d4e12fd2d411abe4abcb61716dc
ArmNorapat <Norapat.Labc@gmail.com>
2022-06-22 23:12:17 +0700

    Update comment on client script

--- CSharpSocket/ClientSide/UDPClient.cs
+++ CSharpSocket/ClientSide/UDPClient.cs
@@ -11,15 +11,15 @@ namespace app
 
         static void Main(string[] args)
         {
-            var client = new UdpClient(); //Server client
-            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), serverPort); // endpoint where server is listening
-            client.Connect(ep);
+            var client = new UdpClient(); // Create client
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), serverPort); // Endpoint where server is listening
+            client.Connect(ep); //Connect to endpoint according IPEndPointObject
 
             Console.WriteLine($"Send : {args[0]}");
             byte[] sendBuffer = Encoding.ASCII.GetBytes(args[0]); //Convert to byte
             client.Send(sendBuffer, sendBuffer.Length); // send data
 
-            // then receive data
+            // Receive data from server
             var receivedData = client.Receive(ref ep);
             var receivedMessage = Encoding.ASCII.GetString(receivedData);
             Console.WriteLine($"Receive data from {ep.ToString()} : {receivedMessage}");
